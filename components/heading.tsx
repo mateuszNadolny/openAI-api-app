bf0fd43903dddaa4b5f38f698da89a1aa8e1917d
mateuszNadolny <mateusznadolny00@gmail.com>
2023-08-02 18:08:49 +0200

    created heading

--- components/heading.tsx
+++ components/heading.tsx
@@ -1,13 +1,30 @@
 import { HeadingProps } from '@/types'
 
+import { cn } from '@/lib/utils'
+// 01:26 conversation AI UI
+
 const Heading = ({
     title,
     description,
-    icon,
+    icon: Icon,
     iconColor,
     bgColor,
 }: HeadingProps) => {
-    return <div>Heading</div>
+    return (
+        <>
+            <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
+                <div className={cn('p-2 w-fit rounded-md', bgColor)}>
+                    <Icon className={cn('w-10 h-10', iconColor)} />
+                </div>
+                <div>
+                    <h2 className="text-3xl font-bold">{title}</h2>
+                    <p className="text-sm text-muted-foreground">
+                        {description}
+                    </p>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Heading
